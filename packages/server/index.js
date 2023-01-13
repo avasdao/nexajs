55c5d76069be750e4ea86cf3eeed1ff8482e9b0c
Shomari <nyusternie@sdot.io>
2023-01-12 20:49:27 -0500

    Create a simple Express web server.

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,1 +1,22 @@
-console.log('Starting Nexa Server...')
+/* Import modules. */
+import express from 'express'
+
+// TODO: Add ascii art.
+console.log(`
+  Starting Nexa Server...`)
+
+/* Initialize Express server. */
+const app = express()
+
+/* Set port. */
+const port = 3000
+
+/* Initialize root endpoint. */
+app.get('/', (req, res) => {
+    res.send('Hello Nexa!')
+})
+
+/* Start listening for incoming connections. */
+app.listen(port, () => {
+    console.log(`\n  Your app is now running here --> http://127.0.0.1:${port}`)
+})
