db8b547d4a973d6925fabd2177bb809bb6410d60
Shomari <nyusternie@sdot.io>
2023-03-08 12:24:12 -0500

    Add "web-friendly" distribution folder, plus build script.

---
+++ lib/scripts/build-dist.sh
@@ -0,0 +1,23 @@
+echo
+
+echo "  Building browser-friendly packages for CDN..."
+echo "  ---------------------------------------------"
+echo
+
+echo "  Building [ Address ] package..."
+browserify ../../packages/Address/index.js -p esmify > ../dist/address.js
+echo "    - address.js"
+echo "    - address.min.js"
+echo "  done."
+echo
+
+echo "  Building [ Utilities ] package..."
+browserify ../../packages/Utils/index.js -p esmify > ../dist/utils.js
+echo "    - utils.js"
+echo "    - utils.min.js"
+echo "  done."
+echo
+
+echo "  All packages were built successfully,"
+echo "  and are now available in the [ /dist ] folder."
+echo
