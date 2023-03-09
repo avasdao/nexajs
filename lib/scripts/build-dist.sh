c47dc2a28630c1a6aa7d133edc1b5bb3fb9af355
Shomari <nyusternie@sdot.io>
2023-03-09 00:35:36 -0500

    Add Nexa (Core) library to build script.

--- lib/scripts/build-dist.sh
+++ lib/scripts/build-dist.sh
@@ -4,19 +4,29 @@ echo "  Building browser-friendly packages for CDN..."
 echo "  ---------------------------------------------"
 echo
 
+cd ..
+
 echo "  Building [ Address ] package..."
-browserify ../../packages/Address/index.js -p esmify > ../dist/address.js
+browserify ../packages/Address/index.js -p esmify > ./dist/address.js
 echo "    - address.js"
-browserify ../../packages/Address/index.js -p esmify | uglifyjs -c > ../dist/address.min.js
+browserify ../packages/Address/index.js -p esmify | uglifyjs -c > ./dist/address.min.js
 echo "    - address.min.js"
 echo "  done."
 echo
 
 echo "  Building [ Utilities ] package..."
-browserify ../../packages/Utils/index.js -p esmify > ../dist/utils.js
+browserify ../packages/Utils/index.js -p esmify > ./dist/utils.js
 echo "    - utils.js"
-browserify ../../packages/Utils/index.js -p esmify | uglifyjs -c > ../dist/utils.min.js
+browserify ../packages/Utils/index.js -p esmify | uglifyjs -c > ./dist/utils.min.js
 echo "    - utils.min.js"
+echo "  done."
+echo
+
+echo "  Building [ Nexa (Core) ] package..."
+browserify ./index.js -p esmify > ./dist/nexa.js
+echo "    - nexa.js"
+browserify ./index.js -p esmify | uglifyjs -c > ./dist/nexa.min.js
+echo "    - nexa.min.js"
 echo "  done."
 echo
 
