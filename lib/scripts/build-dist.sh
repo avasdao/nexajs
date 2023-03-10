d47f418ede8b3d3b82a09e635454f130663729da
Shomari <nyusternie@sdot.io>
2023-03-10 13:00:32 -0500

    Add Rostrum to pre-built packages.

--- lib/scripts/build-dist.sh
+++ lib/scripts/build-dist.sh
@@ -4,13 +4,29 @@ echo "  Building browser-friendly packages for CDN..."
 echo "  ---------------------------------------------"
 echo
 
+echo "  Moving to Core library root folder..."
 cd ..
+echo
+
+echo "  Restore pre-built packages..."
+echo
+rm -f yarn.lock
+yarn
+echo
 
 echo "  Building [ Address ] package..."
 browserify ../packages/Address/index.js -p esmify > ./dist/address.js
 echo "    - address.js"
 browserify ../packages/Address/index.js -p esmify | uglifyjs -c > ./dist/address.min.js
 echo "    - address.min.js"
+echo "  done."
+echo
+
+echo "  Building [ Rostrum ] package..."
+browserify ../packages/Rostrum/index.js -p esmify > ./dist/rostrum.js
+echo "    - rostrum.js"
+browserify ../packages/Rostrum/index.js -p esmify | uglifyjs -c > ./dist/rostrum.min.js
+echo "    - rostrum.min.js"
 echo "  done."
 echo
 
