5decba5af99423b3dab0df23548fc8a9ac5b628f
Shomari <nyusternie@sdot.io>
2023-03-30 05:21:28 -0400

    Add `watchAddress` test.

---
+++ packages/Address/src/watchAddress.js
@@ -0,0 +1,49 @@
+import { createClient } from 'graphql-ws'
+import WebSocket from 'ws'
+
+/* Setup (non-ESM) debugger. */
+import debugFactory from 'debug'
+const debug = debugFactory('nexa:address:subscribe')
+
+export default async (_params, _notif) => {
+    console.log('PARAMS', _params)
+    console.log('NOTIF', _notif)
+    /* Initialize locals. */
+    // let notif
+
+    /* Handle notifications. */
+    // if (_params?.notif) {
+    //     notif = _params.notif
+    // } else if (_notif) {
+    //     notif = _notif
+    // }
+
+    const client = createClient({
+    	url: 'wss://nexa.sh/graphql',
+        webSocketImpl: WebSocket,
+    })
+
+    // subscription
+    const onNext = (_val) => {
+        /* handle incoming values */
+        console.log('TX', _val)
+    }
+
+    let unsubscribe = () => {
+        /* complete the subscription */
+        console.log('done!')
+    }
+
+    await new Promise((resolve, reject) => {
+        unsubscribe = client.subscribe(
+            {
+                query: 'subscription { transaction { txidem hex } }',
+            },
+            {
+                next: onNext,
+                error: reject,
+                complete: resolve,
+            },
+        )
+    }).catch(err => console.error(err))
+}
