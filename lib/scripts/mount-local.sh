716e27183d4259cbf83629e3b8751a092bee9fbe
Shomari <nyusternie@sdot.io>
2023-04-06 03:46:04 -0400

    Add new packages to mounts.

--- lib/scripts/mount-local.sh
+++ lib/scripts/mount-local.sh
@@ -3,11 +3,17 @@ echo "  Cleaning up local mounts..."
 
 rm -rf ../node_modules/@nexajs/address
 rm -rf ../node_modules/@nexajs/blockchain
+rm -rf ../node_modules/@nexajs/crypto
+rm -rf ../node_modules/@nexajs/graph
 rm -rf ../node_modules/@nexajs/hdnode
 rm -rf ../node_modules/@nexajs/id
+rm -rf ../node_modules/@nexajs/ledger
+rm -rf ../node_modules/@nexajs/privacy
 rm -rf ../node_modules/@nexajs/purse
 rm -rf ../node_modules/@nexajs/rostrum
 rm -rf ../node_modules/@nexajs/rpc
+rm -rf ../node_modules/@nexajs/swap
+rm -rf ../node_modules/@nexajs/token
 rm -rf ../node_modules/@nexajs/transaction
 rm -rf ../node_modules/@nexajs/utils
 rm -rf ../node_modules/@nexajs/wallet
@@ -20,11 +26,17 @@ echo
 echo "  Creating local mounts..."
 ln -s ../../../packages/Address ../node_modules/@nexajs/address
 ln -s ../../../packages/Blockchain ../node_modules/@nexajs/blockchain
+ln -s ../../../packages/Crypto ../node_modules/@nexajs/crypto
+ln -s ../../../packages/Graph ../node_modules/@nexajs/graph
 ln -s ../../../packages/Hdnode ../node_modules/@nexajs/hdnode
 ln -s ../../../packages/Id ../node_modules/@nexajs/id
+ln -s ../../../packages/Ledger ../node_modules/@nexajs/ledger
+ln -s ../../../packages/Privacy ../node_modules/@nexajs/privacy
 ln -s ../../../packages/Purse ../node_modules/@nexajs/purse
 ln -s ../../../packages/Rostrum ../node_modules/@nexajs/rostrum
 ln -s ../../../packages/Rpc ../node_modules/@nexajs/rpc
+ln -s ../../../packages/Swap ../node_modules/@nexajs/swap
+ln -s ../../../packages/Token ../node_modules/@nexajs/token
 ln -s ../../../packages/Transaction ../node_modules/@nexajs/transaction
 ln -s ../../../packages/Utils ../node_modules/@nexajs/utils
 ln -s ../../../packages/Wallet ../node_modules/@nexajs/wallet
