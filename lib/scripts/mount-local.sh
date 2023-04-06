8aacdf937e4e1a46b7ac44823f41244317471410
Shomari <nyusternie@sdot.io>
2023-04-06 12:38:26 -0400

    Update Core packages.

--- lib/scripts/mount-local.sh
+++ lib/scripts/mount-local.sh
@@ -4,12 +4,12 @@ echo "  Cleaning up local mounts..."
 rm -rf ../node_modules/@nexajs/address
 rm -rf ../node_modules/@nexajs/blockchain
 rm -rf ../node_modules/@nexajs/crypto
-rm -rf ../node_modules/@nexajs/graph
 rm -rf ../node_modules/@nexajs/hdnode
 rm -rf ../node_modules/@nexajs/id
 rm -rf ../node_modules/@nexajs/ledger
 rm -rf ../node_modules/@nexajs/privacy
 rm -rf ../node_modules/@nexajs/purse
+rm -rf ../node_modules/@nexajs/request
 rm -rf ../node_modules/@nexajs/rostrum
 rm -rf ../node_modules/@nexajs/rpc
 rm -rf ../node_modules/@nexajs/swap
@@ -27,12 +27,12 @@ echo "  Creating local mounts..."
 ln -s ../../../packages/Address ../node_modules/@nexajs/address
 ln -s ../../../packages/Blockchain ../node_modules/@nexajs/blockchain
 ln -s ../../../packages/Crypto ../node_modules/@nexajs/crypto
-ln -s ../../../packages/Graph ../node_modules/@nexajs/graph
 ln -s ../../../packages/Hdnode ../node_modules/@nexajs/hdnode
 ln -s ../../../packages/Id ../node_modules/@nexajs/id
 ln -s ../../../packages/Ledger ../node_modules/@nexajs/ledger
 ln -s ../../../packages/Privacy ../node_modules/@nexajs/privacy
 ln -s ../../../packages/Purse ../node_modules/@nexajs/purse
+ln -s ../../../packages/Request ../node_modules/@nexajs/request
 ln -s ../../../packages/Rostrum ../node_modules/@nexajs/rostrum
 ln -s ../../../packages/Rpc ../node_modules/@nexajs/rpc
 ln -s ../../../packages/Swap ../node_modules/@nexajs/swap
