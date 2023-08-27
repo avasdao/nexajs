7d35d591bf4e95600ff3d03fd84572e98ffe3e79
Shomari <nyusternie@sdot.io>
2023-08-27 06:42:09 -0400

    Update intro.

--- studio/templates/nuxt-ts/pages/index.vue
+++ studio/templates/nuxt-ts/pages/index.vue
@@ -34,14 +34,23 @@
                         <div class="mx-auto max-w-2xl gap-x-14 lg:mx-0 lg:flex lg:max-w-none lg:items-center">
                             <div class="w-full max-w-xl lg:shrink-0 xl:max-w-2xl">
                                 <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                                    Introducing a <span class="text-7xl text-amber-400 italic font-light">Re-imagined</span> Web3 Builder <span class="text-7xl text-amber-400 italic font-light">Experience</span>
+                                    A premium <span class="text-7xl text-amber-400 italic font-light">Builder Experience</span> ideal for Noob thru Full-stack
                                 </h1>
+
+                                <p class="relative mt-6 text-2xl leading-8 font-light text-gray-600 sm:max-w-md lg:max-w-none">
+                                    It DOES NOT matter what your experience level is coming in; our community is ready, willing and able to prepare you for a journey of a lifetime.
+                                </p>
+
+                                <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
+                                    Nexa Studio presents a premium Builder Experience (BX) that will leave you speechless.
+                                </p>
 
                                 <p class="relative mt-6 text-lg leading-8 text-gray-600 sm:max-w-md lg:max-w-none">
                                     Cupidatat minim id magna ipsum sint dolor qui. Sunt sit in quis cupidatat mollit aute velit. Et labore commodo nulla aliqua proident mollit ullamco exercitation tempor. Sint aliqua anim nulla sunt mollit id
                                     pariatur in voluptate cillum. Eu voluptate tempor esse minim amet fugiat veniam occaecat aliqua.
                                 </p>
                             </div>
+
                             <div class="mt-14 flex justify-end gap-8 sm:-mt-44 sm:justify-start sm:pl-20 lg:mt-0 lg:pl-0">
                                 <div class="ml-auto w-44 flex-none space-y-8 pt-32 sm:ml-0 sm:pt-80 lg:order-last lg:pt-36 xl:order-none xl:pt-80">
                                     <div class="relative">
