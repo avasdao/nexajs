072b5a84ef55be49aedd86dba0cdfb3262fd351f
Shomari <nyusternie@sdot.io>
2023-08-29 01:12:39 -0400

    Allow image preview.

--- studio-nft/pages/index.vue
+++ studio-nft/pages/index.vue
@@ -13,6 +13,40 @@ useHead({
 import { useSystemStore } from '@/stores/system'
 const System = useSystemStore()
 
+const imagePreviewUrl = ref(null)
+const imageData = ref(null)
+
+
+const handleChange = async (e) => {
+    const file = e
+    console.log('FILE', file)
+    console.log('FILE (files):', file.files)
+    const input = e.target
+    console.log('INPUT', input)
+
+    if (!file) {
+        return
+    }
+
+    // const readData = (f) => {
+    //     new Promise(resolve => {
+    //         const reader = new FileReader()
+    //         reader.onloadend = () => resolve(reader.result)
+    //         reader.readAsDataURL(f)
+    //     })
+    // }
+
+    // imagePreviewUrl.value = await readData(file)
+
+    if (input.files) {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+            imagePreviewUrl.value = e.target.result
+        }
+        imageData.value = input.files[0]
+        reader.readAsDataURL(input.files[0])
+    }
+}
 
 const mint = () => {
     const json = {
@@ -155,7 +189,12 @@ const mint = () => {
                         <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Cover photo</label>
                         <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                             <div class="text-center">
-                                <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                                <img
+                                    v-if="imagePreviewUrl"
+                                    :src="imagePreviewUrl"
+                                    class="w-fit h-auto"
+                                />
+                                <svg v-else class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
                                     <path
                                         fill-rule="evenodd"
                                         d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
@@ -168,10 +207,18 @@ const mint = () => {
                                         class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                                     >
                                         <span>Upload a file</span>
-                                        <input id="file-upload" name="file-upload" type="file" class="sr-only" />
+                                        <input
+                                            id="file-upload"
+                                            name="file-upload"
+                                            type="file"
+                                            @change="handleChange"
+                                            class="sr-only"
+                                        />
                                     </label>
+
                                     <p class="pl-1">or drag and drop</p>
                                 </div>
+
                                 <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
                             </div>
                         </div>
