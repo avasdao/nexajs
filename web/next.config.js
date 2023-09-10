6d86f5b79721b32b9d25460dbf73a0f47c6882b8
Shomari <nyusternie@sdot.io>
2023-09-09 20:01:10 -0400

    Fix syntax.

--- web/next.config.js
+++ web/next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
     unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_MATOMO_URL,: process.env.NEXT_PUBLIC_MATOMO_URL,
-    NEXT_PUBLIC_MATOMO_SITE_ID,: process.env.NEXT_PUBLIC_MATOMO_SITE_ID,
+    NEXT_PUBLIC_MATOMO_URL: process.env.NEXT_PUBLIC_MATOMO_URL,
+    NEXT_PUBLIC_MATOMO_SITE_ID: process.env.NEXT_PUBLIC_MATOMO_SITE_ID,
   },
 }
 
