5261c0c360e3e825c9f182b701ad5aebc31ad32f
Shomari <nyusternie@sdot.io>
2023-10-15 17:35:45 -0400

    Refactor Wallet `updateBalances`.

--- packages/Wallet/index.js
+++ packages/Wallet/index.js
@@ -4,8 +4,6 @@ const debug = debugFactory('nexa:wallet')
 
 /* Import modules. */
 import { EventEmitter } from 'events'
-import fetch from 'cross-fetch'
-import numeral from 'numeral'
 
 /* Import (library) modules. */
 import {
@@ -44,6 +42,7 @@ import _getDerivationPath from './src/getDerivationPath.js'
 import _parseDerivationPath from './src/parseDerivationPath.js'
 import _send from './src/send.js'
 import _updateAssets from './src/updateAssets.js'
+import _updateBalances from './src/updateBalances.js'
 
 /**
  * Wallet Status
@@ -89,9 +88,6 @@ const DEFAULT_COIN_TYPE = `29223'` // (0x7227) Nexa (https://spec.nexa.org)
 const DEFAULT_ACCOUNT_IDX = `0'`
 const DEFAULT_CHANGE = '0'
 const DEFAULT_ADDRESS_IDX = '0'
-
-/* Set (general) constants. */
-const BALANCE_UPDATE_DELAY = 30000 // 30 seconds
 
 
 /**
@@ -520,140 +516,8 @@ export class Wallet extends EventEmitter {
         return _updateAssets.bind(this)(_subscribe, _fiat)
     }
 
-    /**
-     * Update Balances
-     *
-     * Retrieve balances for ALL wallet assets, ie.
-     *   - coins / satoshis
-     *   - all tokens
-     *
-     * (Optionally) convert all asset values to fiat.
-     */
-    async updateBalances(_fiat = 'USD') {
-// console.log('UPDATE BALANCES (fiat):', _fiat)
-
-        /* Initialize locals. */
-        let coins
-        let coinsTotal
-        let fiat
-        let fiatUSD
-        let markets
-        let price
-        let response
-        let satoshis
-        let ticker
-        // let token
-        let tokenid
-        let tokenList
-        let tokens
-
-        /* Validate markets. */
-        if (!this.markets) {
-            /* Initialize markets. */
-            this._markets = {}
-        }
-
-        // console.log('ASSETS', this.assets)
-
-        /* Requet NEXA ticker. */
-        response = await fetch('https://nexa.exchange/ticker')
-            .catch(err => console.error(err))
-
-        /* Request JSON. */
-        this._markets['NEXA'] = await response.json()
-        // console.log('MARKETS', this._markets)
-
-        /* Set (ticker) price. */
-        price = Number(this._markets['NEXA'].quote.USD.price)
-        // console.log('PRICE', price)
-
-        /* Calculate total coins. */
-        // console.log('COINS', this.coins)
-        coinsTotal = this.coins.reduce(
-            (_total, _coins) => (_total + _coins.satoshis), BigInt(0)
-        )
-
-        /* Build coins bundle. */
-        coins = {
-            amount: Number(coinsTotal) / 100.0,
-            satoshis: coinsTotal,
-        }
-
-        /* Validate fiat value(s). */
-        if (_fiat) {
-            /* Set USD. */
-            fiatUSD = Number(coinsTotal) / 100 // convert to amount
-            fiatUSD = fiatUSD * price // calculate USD value
-            fiatUSD = numeral(fiatUSD).format('0,0.00[000000]') // format value
-            fiatUSD = parseFloat(fiatUSD) // conver to decimal
-
-            /* Set fiat value. */
-            coins.fiat = {
-                'USD': fiatUSD,
-            }
-        }
-
-        /* Initialize tokens bundle. */
-        tokens = {}
-
-        this.tokens.forEach(_token => {
-            if (!tokens[_token.tokenidHex]) {
-                /* Initialize token. */
-                tokens[_token.tokenidHex] = {
-                    amount: BigInt(0),
-                    satoshis: BigInt(0),
-                }
-            }
-
-            /* Add tokens to total. */
-            tokens[_token.tokenidHex].amount += _token.tokens
-
-            /* Add satoshis to total. */
-            tokens[_token.tokenidHex].satoshis += _token.satoshis
-        })
-
-        if (_fiat) {
-            tokenList = []
-            /* Handle each token (fiat) conversion. */
-            Object.keys(tokens).forEach(_tokenid => {
-                tokenList.push(_tokenid)
-            })
-            // console.log('TOKEN LIST', tokenList)
-
-            for (let i = 0; i < tokenList.length; i++) {
-                tokenid = tokenList[i]
-
-                tokens[tokenid].fiat = {}
-
-                if (!tokens[tokenid].fiat['USD']) {
-                    response = await fetch(`https://nexa.exchange/v1/ticker/quote/${tokenid}`)
-                        .catch(err => console.error(err))
-
-                    ticker = await response.json()
-                    // console.log('TICKER', ticker)
-
-                    tokens[tokenid].fiat['USD'] = ticker
-                }
-            }
-        }
-
-        /* Save balances. */
-        this._balances = {
-            coins,
-            tokens,
-        }
-        // console.log('UPDATED BALANCES (coins):', this._balances.coins)
-        // console.log('UPDATED BALANCES (tokens):', this._balances.tokens)
-
-        /* Set a timeout (delay) for the next update. */
-        // NOTE: Use an "arrow function" to resolve (this) issue.
-        setTimeout(() => {
-            /* Update balances. */
-            this.updateBalances(_fiat)
-        }, BALANCE_UPDATE_DELAY)
-
-        /* Return balances. */
-        return this._balances
+    async updateBalances(_fiat) {
+        return _updateBalances.bind(this)(_fiat)
     }
 
     toObject() {
