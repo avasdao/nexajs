c057ff858d9d73ccf2f98ff48f3a41a39c9765b4
Shomari <nyusternie@sdot.io>
2023-10-16 05:06:57 -0400

    Add components to all pages.

--- studio-wallet/pages/history.vue
+++ studio-wallet/pages/history.vue
@@ -5,30 +5,8 @@ useHead({
         { name: 'description', content: `Nexa Studio makes building your next BIG idea effortless.` }
     ],
 })
-
-/* Initialize stores. */
-import { useSystemStore } from '@/stores/system'
-const System = useSystemStore()
-
-// onMounted(() => {
-//     console.log('Mounted!')
-//     // Now it's safe to perform setup operations.
-// })
-
-// onBeforeUnmount(() => {
-//     console.log('Before Unmount!')
-//     // Now is the time to perform all cleanup operations.
-// })
 </script>
 
 <template>
-    <main class="flex flex-col gap-4">
-        <h1 class="text-5xl font-medium">
-            History
-        </h1>
-
-        <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eius voluptatem minus natus at eveniet dolorum eos mollitia, maxime animi excepturi harum omnis illum odit recusandae pariatur! Unde, explicabo molestias.
-        </p>
-    </main>
+    <ProfileWalletHistory isFullScreen="true" />
 </template>
