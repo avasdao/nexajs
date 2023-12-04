c90562382540c867f4eab2d43c84ecc3d7a6283a
Shomari <nyusternie@sdot.io>
2023-12-04 06:46:12 -0500

    Update to latest Script.

--- packages/Script/index.js
+++ packages/Script/index.js
@@ -2,6 +2,9 @@
 import debugFactory from 'debug'
 const debug = debugFactory('nexa:script')
 
+/* Setup environment. */
+import 'dotenv/config'
+
 /* Import (local) modules. */
 import _decodeNullData from './src/decodeNullData.js'
 import _encodeDataPush from './src/encodeDataPush.js'
