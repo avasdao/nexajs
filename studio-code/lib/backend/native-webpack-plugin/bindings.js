0be07d24baec15910d479cfe5e198918b72211a2
Shomari <nyusternie@sdot.io>
2024-07-09 02:23:02 -0400

    Remove lib from repo.

--- studio-code/lib/backend/native-webpack-plugin/bindings.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (jsModule) {
-    switch (jsModule) {
-        case 'drivelist': return require('drivelist/build/Release/drivelist.node');
-    }
-    throw new Error(`unhandled module: "${jsModule}"`);
-}
--- studio-code/lib/backend/native-webpack-plugin/ripgrep.js
+++
@@ -1,4 +0,0 @@
-
-const path = require('path');
-
-exports.rgPath = path.join(__dirname, `./native/rg${process.platform === 'win32' ? '.exe' : ''}`);
