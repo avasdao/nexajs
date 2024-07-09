fdc6f85b3a021eeedeabc449b76da24b99d2c92b
Shomari <nyusternie@sdot.io>
2024-07-09 11:40:17 -0400

    Remove `crypto` from dependencies.

--- packages/Crypto/libs/hash.js
+++ packages/Crypto/libs/hash.js
@@ -1,45 +1,16 @@
-import crypto from 'crypto'
 import BufferUtil from '../util/buffer.js'
 import $ from '../util/preconditions.js'
+import { sha256 } from '../index.js'
 
 /* Initialize hash. */
 let Hash = {}
 
-Hash.sha1 = function(buf) {
-    $.checkArgument(BufferUtil.isBuffer(buf))
-    return crypto.createHash('sha1').update(buf).digest()
-}
-
-Hash.sha1.blocksize = 512
-
 Hash.sha256 = function(buf) {
     $.checkArgument(BufferUtil.isBuffer(buf))
-    return crypto.createHash('sha256').update(buf).digest()
+    return Buffer.from(sha256(buf))
 }
 
 Hash.sha256.blocksize = 512
-
-Hash.sha256sha256 = function(buf) {
-    $.checkArgument(BufferUtil.isBuffer(buf))
-    return Hash.sha256(Hash.sha256(buf))
-}
-
-Hash.ripemd160 = function(buf) {
-    $.checkArgument(BufferUtil.isBuffer(buf))
-    return crypto.createHash('ripemd160').update(buf).digest()
-}
-
-Hash.sha256ripemd160 = function(buf) {
-    $.checkArgument(BufferUtil.isBuffer(buf))
-    return Hash.ripemd160(Hash.sha256(buf))
-}
-
-Hash.sha512 = function(buf) {
-    $.checkArgument(BufferUtil.isBuffer(buf))
-    return crypto.createHash('sha512').update(buf).digest()
-}
-
-Hash.sha512.blocksize = 1024
 
 Hash.hmac = function(hashf, data, key) {
     //http://en.wikipedia.org/wiki/Hash-based_message_authentication_code
@@ -77,10 +48,6 @@ Hash.hmac = function(hashf, data, key) {
 
 Hash.sha256hmac = function(data, key) {
     return Hash.hmac(Hash.sha256, data, key)
-}
-
-Hash.sha512hmac = function(data, key) {
-    return Hash.hmac(Hash.sha512, data, key)
 }
 
 export default Hash
