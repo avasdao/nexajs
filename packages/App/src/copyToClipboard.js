fe1259b8e2d8045bf5dd351223b4b3968e2c3196
Shomari <nyusternie@sdot.io>
2024-07-13 16:26:26 -0400

    Add validation to "client-only" methods, eg. `copyToClipboard`.

--- packages/App/src/copyToClipboard.js
+++ packages/App/src/copyToClipboard.js
@@ -1,46 +1,55 @@
 export default () => {
+    /* Validate client. */
     // NOTE: We ONLY run this on the (web) client.
-    window.Clipboard = (function(window, document, navigator) {
-        let textArea,
-            copy
+    if (
+        typeof window !== 'undefined' &&
+        typeof document !== 'undefined' &&
+        typeof navigator !== 'undefined'
+    ) {
+        /* Inject new object into `window`. */
+        window.Clipboard = (function (window, document, navigator) {
+            /* Initialize locals. */
+            let copy
+            let textArea
 
-        function isOS() {
-            return navigator.userAgent.match(/ipad|iphone/i)
-        }
-
-        function createTextArea(text) {
-            textArea = document.createElement('textArea')
-            textArea.value = text
-            document.body.appendChild(textArea)
-        }
-
-        function selectText() {
-            let range,
-                selection
-
-            if (isOS()) {
-                range = document.createRange()
-                range.selectNodeContents(textArea)
-                selection = window.getSelection()
-                selection.removeAllRanges()
-                selection.addRange(range)
-                textArea.setSelectionRange(0, 999999)
-            } else {
-                textArea.select()
+            function isOS() {
+                return navigator.userAgent.match(/ipad|iphone/i)
             }
-        }
 
-        function _copyToClipboard() {
-            document.execCommand('copy')
-            document.body.removeChild(textArea)
-        }
+            function createTextArea(text) {
+                textArea = document.createElement('textArea')
+                textArea.value = text
+                document.body.appendChild(textArea)
+            }
 
-        copy = function(text) {
-            createTextArea(text)
-            selectText()
-            _copyToClipboard()
-        }
+            function selectText() {
+                let range,
+                    selection
 
-        return { copy }
-    })(window, document, navigator)
+                if (isOS()) {
+                    range = document.createRange()
+                    range.selectNodeContents(textArea)
+                    selection = window.getSelection()
+                    selection.removeAllRanges()
+                    selection.addRange(range)
+                    textArea.setSelectionRange(0, 999999)
+                } else {
+                    textArea.select()
+                }
+            }
+
+            function _copyToClipboard() {
+                document.execCommand('copy')
+                document.body.removeChild(textArea)
+            }
+
+            copy = function (text) {
+                createTextArea(text)
+                selectText()
+                _copyToClipboard()
+            }
+
+            return { copy }
+        })(window, document, navigator)
+    }
 }
