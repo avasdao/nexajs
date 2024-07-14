ff9387658efb84983f984f4980cedb954e90b938
Shomari <nyusternie@sdot.io>
2024-07-13 21:17:28 -0400

    Adjust method params (for consistency).

--- packages/Crypto/src/getHmac.js
+++ packages/Crypto/src/getHmac.js
@@ -1,7 +1,7 @@
 /* Import modules. */
 import CryptoJS from 'crypto-js'
 
-export default (_body, _secret) => {
+export default (_secret, _body) => {
     const signature = CryptoJS.HmacSHA512(_body, _secret).toString()
     // console.log(`HMAC-512 signature: [ ${signature} ]`)
 
