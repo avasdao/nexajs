a0e8afa097e09defffa9d77ea86b85c4da182e50
Shomari <nyusternie@sdot.io>
2024-07-13 23:11:35 -0400

    Migrate to (binary) HMAC algo.

--- packages/Crypto/src/getHmac.js
+++ packages/Crypto/src/getHmac.js
@@ -1,9 +1,45 @@
 /* Import modules. */
-import CryptoJS from 'crypto-js'
+// import CryptoJS from 'crypto-js'
+import { flattenBinArray } from '@nexajs/utils'
 
-export default (_secret, _body) => {
-    const signature = CryptoJS.HmacSHA512(_body, _secret).toString()
-    // console.log(`HMAC-512 signature: [ ${signature} ]`)
+import { sha512 } from '../index.js'
 
-    return signature
+// export default (_secret, _body) => {
+//     const signature = CryptoJS.HmacSHA512(_body, _secret).toString()
+//     // console.log(`HMAC-512 signature: [ ${signature} ]`)
+//
+//     return signature
+// }
+
+export default (_secret, _message, _size = 512) => {
+    /* Calculate byte length. */
+    const blockByteLength = (_size / 4) // 128
+
+    const key = new Uint8Array(blockByteLength)
+        .fill(0)
+
+    // eslint-disable-next-line functional/no-expression-statement
+    key.set(_secret.length > blockByteLength ? sha512(_secret) : _secret, 0)
+
+    const innerPaddingFill = 0x36
+
+    const innerPadding = new Uint8Array(blockByteLength)
+        .fill(innerPaddingFill)
+
+    // eslint-disable-next-line no-bitwise
+    const innerPrefix = innerPadding.map((pad, index) => pad ^ key[index])
+
+    const innerContent = flattenBinArray([ innerPrefix, _message ])
+
+    const innerResult = sha512(innerContent)
+
+    const outerPaddingFill = 0x5c
+
+    const outerPadding = new Uint8Array(blockByteLength)
+        .fill(outerPaddingFill)
+
+    // eslint-disable-next-line no-bitwise
+    const outerPrefix = outerPadding.map((pad, index) => pad ^ key[index])
+
+    return sha512(flattenBinArray([ outerPrefix, innerResult ]))
 }
