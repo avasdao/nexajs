6aaf2efb11c7c1d1b5e16fd42c5e2a42c5a78644
Shomari <nyusternie@sdot.io>
2024-08-17 18:50:49 -0400

    Add new Logger Studio stub.

---
+++ studio/templates/nuxt-ts/pages/logger.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+useHead({
+    title: `Logger — Nexa Studio`,
+    meta: [
+        { name: 'description', content: `A robust logging engine.` }
+    ],
+})
+
+/* Initialize stores. */
+import { useSystemStore } from '@/stores/system'
+const System = useSystemStore()
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+</script>
+
+<template>
+    <main class="max-w-5xl mx-auto py-5 flex flex-col gap-4">
+        <h1 class="text-5xl font-medium">
+            Logger
+        </h1>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eius voluptatem minus natus at eveniet dolorum eos mollitia, maxime animi excepturi harum omnis illum odit recusandae pariatur! Unde, explicabo molestias.
+        </p>
+
+        <p>
+            Launch the Logger Studio:
+            <NuxtLink to="https://logger.nexa.studio" class="block text-2xl font-bold text-blue hover:underline">
+                https://logger.nexa.studio
+            </NuxtLink>
+        </p>
+    </main>
+
+    <Footer />
+</template>
