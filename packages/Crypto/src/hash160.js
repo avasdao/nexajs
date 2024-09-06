60d2f55e664638a12a3eea790907f15453e6a02a
Shomari <nyusternie@sdot.io>
2024-09-06 05:09:40 -0400

    Update to latest Crypto lib.

--- packages/Crypto/src/hash160.js
+++ packages/Crypto/src/hash160.js
@@ -1,3 +1,8 @@
-export default () => {
-    // TODO
+import {
+    ripemd160,
+    sha256,
+} from '../index.js'
+
+export default (_value) => {
+    return ripemd160(sha256(_value))
 }
--- packages/Crypto/src/hash256.js
+++ packages/Crypto/src/hash256.js
@@ -1,3 +1,5 @@
-export default () => {
-    // TODO
+import { sha256 } from '../index.js'
+
+export default (_value) => {
+    return sha256(sha256(_value))
 }
