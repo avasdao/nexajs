c97c50201c7d3098f26e2285909e653fce967c87
Shomari <nyusternie@sdot.io>
2024-09-06 01:21:59 -0400

    Refactor wisebox pages.

---
+++ studio-wisebox/pages/snippet/ttl.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+useHead({
+    title: `Time-To-Live — Wisebox`,
+    meta: [
+        { name: 'description', content: `Wisebox is the Ultimate Playground for UTXO Script developers to design, test and deploy their smart contracts.` }
+    ],
+})
+
+/* Initialize stores. */
+import { useSystemStore } from '@/stores/system'
+const System = useSystemStore()
+
+// onMounted(() => {
+//     console.log('Mounted!')
+//     // Now it's safe to perform setup operations.
+// })
+
+// onBeforeUnmount(() => {
+//     console.log('Before Unmount!')
+//     // Now is the time to perform all cleanup operations.
+// })
+</script>
+
+<template>
+    <main class="flex flex-col gap-4">
+        <h1 class="text-5xl font-medium">
+            Time-To-Live
+        </h1>
+
+        <p>
+            Learn how to expire your contracts and automagically purge them from the network.
+        </p>
+    </main>
+</template>
