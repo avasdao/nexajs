76fbc1ceebabb6fd3631cde0c59965ceea878eaf
Shomari <nyusternie@sdot.io>
2025-01-13 14:13:40 -0500

    Setup standalone build.

--- web/next.config.js
+++ web/next.config.js
@@ -2,6 +2,7 @@ const withMarkdoc = require('@markdoc/next.js')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
   experimental: {
